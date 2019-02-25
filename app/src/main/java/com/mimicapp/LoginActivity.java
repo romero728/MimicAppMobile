8823f44fd0161e214f2027f99367e8f8b78e5410
romero728 <david728romero@gmail.com>
2019-02-25 15:19:44 -0500

    C06 - Implementación de Login con Google - Se ha creado correo de mimicapp - Se ha creado proyecto en Firebase - Se ha añadido archivo json (Google services) - Se ha modificado gradle (Project) - Se ha modificado gradle (Module) - Se ha modificado LoginActivity.java para implementar el inicio de sesión - Se ha eliminado el botón iniciar sesión con Google en activity_login.xml - Se ha añadido el botón de Google (predeterminado) en activity_login.xml

--- app/src/main/java/com/mimicapp/LoginActivity.java
+++ app/src/main/java/com/mimicapp/LoginActivity.java
@@ -1,13 +1,64 @@
 package com.mimicapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 888;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        SignInButton signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        final GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            String userName = account.getDisplayName();
+
+            Toast.makeText(this, userName, Toast.LENGTH_LONG).show();
+        } catch (ApiException e) {
+            e.printStackTrace();
+        }
     }
 }
