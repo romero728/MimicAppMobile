c4f82c633305df23cd5c003fba122c465b06435b
romero728 <david728romero@gmail.com>
2019-02-26 20:30:08 -0500

    C08 - Conexión de Android Studio con servidor remoto (000webhost) - Se crea cuenta en 000webhost - Se crea BD en phpmyadmin - Se modifica LoginActivity para guardar la información del usuario y enviarla a la clase UserRegister - Se crea la clase UserRegister para realizar el registro / login del usuario en la base de datos

--- app/src/main/java/com/mimicapp/LoginActivity.java
+++ app/src/main/java/com/mimicapp/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.mimicapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -13,15 +15,25 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
+import com.mimicapp.DBConnection.UserRegister;
 
 public class LoginActivity extends AppCompatActivity {
 
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor spEditor;
+
     private static final int RC_SIGN_IN = 888;
+
+    String url = "https://mimicapp.000webhostapp.com/mimicApp/";
+    String userEmail = "";
+    String userName = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        sharedPreferences = getSharedPreferences("Preferences", Context.MODE_PRIVATE);
 
         SignInButton signInButton = findViewById(R.id.sign_in_button);
         signInButton.setSize(SignInButton.SIZE_STANDARD);
@@ -54,9 +66,24 @@ public class LoginActivity extends AppCompatActivity {
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
         try {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-            String userName = account.getDisplayName();
 
-            Toast.makeText(this, userName, Toast.LENGTH_LONG).show();
+            if (account != null) {
+                userEmail = account.getEmail();
+                userName = account.getDisplayName();
+
+                spEditor  = sharedPreferences.edit();
+                spEditor.putString("urlKey", url);
+                spEditor.putString("emailKey", userEmail);
+                spEditor.putString("nameKey", userName);
+                spEditor.apply();
+
+                new UserRegister(this).execute(url, userEmail, userName);
+
+                Toast.makeText(this, "Welcome " + userName, Toast.LENGTH_LONG).show();
+
+            } else {
+                Toast.makeText(this, "Ups! Error xD", Toast.LENGTH_LONG).show();
+            }
         } catch (ApiException e) {
             e.printStackTrace();
         }
--- app/src/main/java/com/mimicapp/MainActivity.java
+++ app/src/main/java/com/mimicapp/MainActivity.java
@@ -1,13 +1,34 @@
 package com.mimicapp;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tvUserName = (TextView) findViewById(R.id.tvUserName);
+
+        sharedPreferences = getSharedPreferences("Preferences", Context.MODE_PRIVATE);
+        String userName = sharedPreferences.getString("nameKey", null);
+
+        if (userName != null) {
+            userName = "Hola " + userName;
+            tvUserName.setText(userName);
+        } else {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
     }
 }
