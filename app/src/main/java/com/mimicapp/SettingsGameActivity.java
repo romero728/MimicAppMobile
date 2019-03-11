5f5725818ba8fc84ebab900bad72363861494e56
romero728 <david728romero@gmail.com>
2019-03-11 13:17:56 -0500

    C20 - Cambios en SettingsGameActivity - Se modifica splash screen para dar más tiempo de animación - Se almacenan las opciones de tiempo en el archivo strings.xml - Se modifica layout añadiendo un spinner para elegir el tipo de categoría - Se agrega funcionalidad para ocultar o mostrar el spinner de categorías cuando se elija opción "Todas" en el tipo de categoría - Se agrega la funcionalidad de cambiar la lista de categorías según el tipo seleccionado por el usuario

--- app/src/main/java/com/mimicapp/SettingsGameActivity.java
+++ app/src/main/java/com/mimicapp/SettingsGameActivity.java
@@ -3,6 +3,7 @@ package com.mimicapp;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Build;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +11,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -19,7 +21,15 @@ import java.util.Objects;
 import java.util.regex.Pattern;
 
 public class SettingsGameActivity extends AppCompatActivity {
-    String categorySelected, timeSelected;
+    ConstraintLayout.LayoutParams params;
+
+    String categorySelected, timeSelected, kindCategorySelected;
+
+    Spinner sKindCategory = null;
+    Spinner sCategory = null;
+    Spinner sTime = null;
+
+    List<String> listCategories;
 
     SharedPreferences spSettingsGame;
     SharedPreferences.Editor editorSettingsGame;
@@ -37,17 +47,51 @@ public class SettingsGameActivity extends AppCompatActivity {
 
         spSettingsGame = getSharedPreferences("settingsNewGame", Context.MODE_PRIVATE);
 
-        Spinner sCategory = findViewById(R.id.sSGCategory);
-        Spinner sTime = findViewById(R.id.sSGTime);
+        final TextView tvLabel3 = findViewById(R.id.tvSGLabel3);
+        final TextView tvLabel4 = findViewById(R.id.tvSGLabel4);
+
+        sKindCategory = findViewById(R.id.sSGKindCategory);
+        sCategory = findViewById(R.id.sSGCategory);
+        sTime = findViewById(R.id.sSGTime);
 
         Button btnNext = findViewById(R.id.btnSGBeginPlay);
 
-        List<String> myList = new ArrayList<String>(Arrays.asList(getCategories()));
+        ArrayAdapter<CharSequence> adapterKind = ArrayAdapter.createFromResource(this,
+                R.array.kind_categories, android.R.layout.simple_spinner_item);
+        adapterKind.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sKindCategory.setAdapter(adapterKind);
 
-        ArrayAdapter<String> adapterCategory = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, myList);
-        adapterCategory.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        sCategory.setAdapter(adapterCategory);
+        sKindCategory.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                switch (i) {
+                    case 0:
+                        kindCategorySelected = "all";
+                        hideCategories(tvLabel3, tvLabel4, sCategory);
+
+                        break;
+                    case 1:
+                        kindCategorySelected = "general";
+                        getGeneralCategories();
+                        showCategories(tvLabel3, tvLabel4, sCategory);
+
+                        break;
+                    case 2:
+                        kindCategorySelected = "custom";
+                        getCustomCategories();
+                        showCategories(tvLabel3, tvLabel4, sCategory);
+
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
         sCategory.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -61,10 +105,8 @@ public class SettingsGameActivity extends AppCompatActivity {
             }
         });
 
-        List<String> myTime = new ArrayList<String>(Arrays.asList(getTime()));
-
-        ArrayAdapter<String> adapterTime = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, myTime);
+        ArrayAdapter<CharSequence> adapterTime = ArrayAdapter.createFromResource(this,
+                R.array.time_options, android.R.layout.simple_spinner_item);
         adapterTime.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         sTime.setAdapter(adapterTime);
 
@@ -84,6 +126,7 @@ public class SettingsGameActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 editorSettingsGame = spSettingsGame.edit();
+                editorSettingsGame.putString("kindCategoryKey", kindCategorySelected);
                 editorSettingsGame.putString("categoryKey", categorySelected);
                 editorSettingsGame.putString("timeKey", timeSelected);
                 editorSettingsGame.apply();
@@ -91,25 +134,58 @@ public class SettingsGameActivity extends AppCompatActivity {
         });
     }
 
-    public String[] getCategories() {
-        String listCategories = getIntent().getExtras().getString("listCategories");
-        String splitter = Pattern.quote("|");
-        String[] categoryArray = listCategories.split(splitter);
-        categoryArray[0] = "Todas";
+    public void getGeneralCategories() {
+        listCategories = new ArrayList<>(Arrays.asList(getCategories(1)));
 
-        return categoryArray;
+        ArrayAdapter<String> adapterCategory = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, listCategories);
+        adapterCategory.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sCategory.setAdapter(adapterCategory);
     }
 
-    public String[] getTime() {
-        String[] timeArray = new String[]{
-            "15 segundos",
-            "30 segundos",
-            "45 segundos",
-            "1 minuto",
-            "3 minutos",
-            "5 minutos"
-        };
+    public void getCustomCategories() {
+        listCategories = new ArrayList<>(Arrays.asList(getCategories(2)));
 
-        return timeArray;
+        ArrayAdapter<String> adapterCategory = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, listCategories);
+        adapterCategory.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sCategory.setAdapter(adapterCategory);
+    }
+
+    public String[] getCategories(int k) {
+        String splitKind = Pattern.quote("/");
+        String splitCategory = Pattern.quote("|");
+
+        String listCategories = getIntent().getExtras().getString("listCategories");
+
+        String[] allCategories = listCategories.split(splitKind);
+
+        String[] generalCategories = allCategories[0].split(splitCategory);
+        String[] customCategories = allCategories[1].split(splitCategory);
+
+        generalCategories[0] = "Todas";
+        customCategories[0] = "Todas";
+
+        if (k == 1) {
+            return generalCategories;
+        } else {
+            return customCategories;
+        }
+    }
+
+    public void showCategories(TextView tv3, TextView tv4, Spinner s) {
+        tv3.setVisibility(View.VISIBLE);
+        s.setVisibility(View.VISIBLE);
+
+        params = (ConstraintLayout.LayoutParams) tv4.getLayoutParams();
+        params.topToBottom = R.id.sSGCategory;
+    }
+
+    public void hideCategories(TextView tv3, TextView tv4, Spinner s) {
+        tv3.setVisibility(View.INVISIBLE);
+        s.setVisibility(View.INVISIBLE);
+
+        params = (ConstraintLayout.LayoutParams) tv4.getLayoutParams();
+        params.topToBottom = R.id.sSGKindCategory;
     }
 }
--- app/src/main/java/com/mimicapp/SplashScreenActivity.java
+++ app/src/main/java/com/mimicapp/SplashScreenActivity.java
@@ -8,7 +8,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class SplashScreenActivity extends AppCompatActivity {
-    private static final long SPLASH_SCREEN_DELAY = 3000;
+    private static final long SPLASH_SCREEN_DELAY = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
